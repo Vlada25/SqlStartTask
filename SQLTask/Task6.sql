2084d42bf6ff21095198f8101d0dad03519507e8
Vlada <aladka03@gmail.com>
2022-01-31 10:53:27 +0300

    Simpified code in Task6

--- SQLTask/Task6.sql
+++ SQLTask/Task6.sql
@@ -1,15 +1,15 @@
 USE DB_Bank
 
 SELECT Clients.FullName,
-	AVG(TotalBalance.SumBalance) AS FullBalance,
-	COALESCE(SUM(Cards.CardBalance), 0) AS CardsSum,
-	COALESCE(AVG(TotalBalance.SumBalance) - SUM(Cards.CardBalance), AVG(TotalBalance.SumBalance)) AS AvailableMoney
-FROM (SELECT Clients.FullName, 
-		SUM(Accounts.Balance) AS SumBalance
-	FROM Clients
-	FULL OUTER JOIN Accounts ON Accounts.ClientId = Clients.Id
-	GROUP BY Clients.FullName) AS TotalBalance
-FULL OUTER JOIN Clients ON Clients.FullName = TotalBalance.FullName
-FULL OUTER JOIN Accounts ON Accounts.ClientId = Clients.Id
-FULL OUTER JOIN Cards ON Cards.AccountNumber = Accounts.AccountNumber
-GROUP BY Clients.Id, Clients.FullName
+	(SELECT SUM(Accounts.Balance)
+	 FROM Accounts
+	 WHERE Accounts.ClientId = Clients.Id) AS FullBalance,
+	(SELECT COALESCE(SUM(Cards.CardBalance), 0) 
+	 FROM Cards
+	 JOIN Accounts ON Accounts.ClientId = Clients.Id
+	 WHERE Cards.AccountNumber = Accounts.AccountNumber) AS CardsSum,
+	(SELECT SUM(DISTINCT Accounts.Balance) - COALESCE(SUM(Cards.CardBalance), 0)
+	 FROM Accounts
+	 LEFT JOIN Cards ON Cards.AccountNumber = Accounts.AccountNumber
+	 WHERE Accounts.ClientId = Clients.Id) AS AvailableMoney
+FROM Clients
